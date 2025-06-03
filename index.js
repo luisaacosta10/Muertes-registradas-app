9733d29f7ae760a42df4ee2cc18335fbe7625f66
luisaacosta10 <luisaacosta454@gmail.com>
2025-06-03 17:39:23 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+
+// Simulación de comportamiento interactivo (placeholder)
+document.getElementById('root').innerHTML = '<h1>Visualización de Muertes Registradas</h1><p>Contenido interactivo pronto disponible.</p>';
